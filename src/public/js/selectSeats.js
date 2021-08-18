d37ceb96eab6a0beb1fd809e3fee24b958748fa9
KabirBhae <67532637+KabirBhae@users.noreply.github.com>
2021-08-18 09:19:36 +0600

    functions made compact

---
+++ src/public/js/selectSeats.js
@@ -0,0 +1,12 @@
+function changeSeatColor(element){
+    element.classList.toggle('filter-green');
+    var desiredClass = element.classList[0].toString(); //class of the img tag obtained from bus ID
+    classArray = desiredClass.split("-");       //extracting the bus ID
+    numberOfSelectedSeats(classArray[1]);   
+}
+
+function numberOfSelectedSeats(ID){
+    var number = document.querySelectorAll('.filter-green').length;
+    document.querySelector(`#numberof-selected-seats-${ID}`).innerText = `Number of seats Selected: ${number}`;
+    document.querySelector(`#total-fare-${ID}`).innerText = `Total Fare: ${400*number}`;
+}
---
+++ src/public/js/viewSeatLayout.js
@@ -0,0 +1,52 @@
+function viewSeats(buttonID){
+    console.log("View Seats button is clicked. ID of the button is",buttonID);
+    
+    const viewLayoutButton = document.getElementById(`${buttonID}`); //this is the button that is clicked to view/hide seats
+    
+    if(viewLayoutButton.innerText === 'View Seats')
+    {   
+        //serial of function calls doesn't matter
+        hideOthers();
+        changeOthersInnerText();   
+        
+        viewLayoutButton.innerText = "Hide Seats"    //changes the innerText of it's own 
+
+        resetGreenSeats();
+        resetNumberOfSelectedSeats();
+    }
+    else{
+        viewLayoutButton.innerText = "View Seats";   
+    }
+
+    var card = document.getElementById(`layout-${buttonID}`);   //default display of bus layout is set to 'none'
+    card.classList.toggle("displayCard");   //displayCard will have display:block
+    
+}
+
+function hideOthers(){              //hides the display of all other seat layouts
+    var toBeRemoved = document.getElementsByClassName("displayCard");   
+    for(var i=0; i<toBeRemoved.length; i++){
+        toBeRemoved[i].classList.remove("displayCard");     
+    }
+}
+
+function changeOthersInnerText(){           //changes the innerText of all other 'Hide seats' buttons
+    var viewOrhideButtons = document.getElementsByClassName("viewOrhide");  
+        for (var i = 0; i<viewOrhideButtons.length;i ++){
+            viewOrhideButtons[i].innerText = "View Seats";   
+        }
+}
+
+function resetNumberOfSelectedSeats(){          //sets the default innerText of 'number of seats selected' div
+    var commonNumberClass = document.getElementsByClassName("commonNumberClass");
+        for(i=0; i<commonNumberClass.length; i++){
+            commonNumberClass[i].innerText = "Number of seats Selected: 0";
+        }
+}
+
+function resetGreenSeats(){             //clears all selected seats
+    var greenSeats = document.querySelectorAll('.filter-green');
+        for(var i = 0; i<greenSeats.length; i++){
+            greenSeats[i].classList.remove("filter-green");
+        }
+}

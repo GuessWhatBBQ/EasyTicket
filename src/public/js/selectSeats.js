ecea2164bb70bc2bba62da9536730f1e232b409c
GuessWhatBBQ <saminslm@gmail.com>
2021-08-31 13:22:57 +0600

    most linter/code style errors fixed

--- src/public/js/selectSeats.js
+++ src/public/js/selectSeats.js
@@ -1,13 +1,15 @@
-function selectSeat(element){
-    element.classList.toggle('filter-green');   //why it appears green
-    
-    var desiredClass = element.classList[0].toString(); //class of the img tag obtained from bus ID
-    var classArray = desiredClass.split("-");       //extracting the bus ID
+function selectSeat(element) {
+    element.classList.toggle('filter-green'); // why it appears green
+
+    const desiredClass = element.classList[0].toString(); // class of the img tag obtained from bus ID
+    const classArray = desiredClass.split('-'); // extracting the bus ID
     numberOfSelectedSeats(classArray[1]);
 }
 
-function numberOfSelectedSeats(ID){
-    var number = document.querySelectorAll('.filter-green').length;
+function numberOfSelectedSeats(ID) {
+    const number = document.querySelectorAll('.filter-green').length;
     document.querySelector(`#numberof-selected-seats-${ID}`).innerText = `Number of seats Selected: ${number}`;
-    document.querySelector(`#total-fare-${ID}`).innerText = `Total Fare: ${400*number}`;
+    document.querySelector(`#total-fare-${ID}`).innerText = `Total Fare: ${400 * number}`;
 }
+
+window.selectSeat = selectSeat;

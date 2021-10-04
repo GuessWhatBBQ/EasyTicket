5290f40554edf97fdf8c820ba1a9561ddd8086a8
KabirBhae <67532637+KabirBhae@users.noreply.github.com>
2021-10-04 22:49:51 +0600

    info of new bus inputted by admin is extreacted.add bus function not implemented.

--- src/public/js/addBus.js
+++ src/public/js/addBus.js
@@ -1,6 +1,22 @@
-function addBus(){
-    console.log('heel');
+document.querySelector('.addBusFrom').addEventListener('submit',function addBus(event){
+    event.preventDefault();
+ 
+    //getting info of new bus to be added
+    const busInfo = {pickup :"", destination :"", startingWeekday :"",startingTime :"", arrivalWeekday :"",arrivalTime :"",seatFare :"",supervisorID :""};
+    
+    let adminBusInput = document.querySelector('.addBusFrom').querySelectorAll("input");
+    busInfo.pickup = adminBusInput[0].value;
+    busInfo.destination = adminBusInput[1].value;
+    busInfo.startingWeekday = adminBusInput[2].value;
+    busInfo.startingTime = adminBusInput[3].value;
+    busInfo.arrivalWeekday = adminBusInput[4].value;
+    busInfo.arrivalTime = adminBusInput[5].value;
+    busInfo.seatFare = adminBusInput[6].value;
+    busInfo.supervisorID = adminBusInput[7].value;
 
-    //clears the previously selected supervisor
-    document.getElementById('supervisorInput').value = '';
-}
+    console.log(busInfo);
+
+    //clears the previously inputted bus info
+    adminBusInput.forEach(element => element.value = '');
+    
+},false);

6acf4afea8e223c19d1f1d74e59d5a883d841866
KabirBhae <67532637+KabirBhae@users.noreply.github.com>
2021-10-08 05:12:06 +0600

    placeholders for seatInfo created

--- src/public/js/admin/selectSeats.js
+++ src/public/js/admin/selectSeats.js
@@ -1,7 +1,22 @@
-function numberOfSelectedSeats(ID) {
-    const number = document.querySelectorAll('.filter-green').length;
-    document.querySelector(`#passengerName-${ID}`).innerText = `Name of passenger: ${number}`;
-    document.querySelector(`#phoneNumber-${ID}`).innerText = `Phone Number ${400 * number}`;
+function getSeatInfo(ID) {
+    var seatInfo = {tripID:`${ID}`, seatNumber:"", firstName:"", lastName:"",phoneNumber:""};
+    seatInfo.seatNumber = document.getElementsByClassName('filter-green')[0].parentNode.innerText;
+    fetch('/api/supervisor/tripinfo', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(seatInfo),
+        })
+        .then(response => response.json())
+        .then(data=>{
+            seatInfo.firstName = data.first_name;
+            seatInfo.lastName = data.last_name;
+            seatInfo.phoneNumber = data.phone_number;
+        })
+    // const number = document.querySelectorAll('.filter-green').length;
+    document.querySelector(`#passengerName-${ID}`).innerText = `Name of passenger: ${seatInfo.firstName}+' '+${seatInfo.lastName}`;
+    document.querySelector(`#phoneNumber-${ID}`).innerText = `Phone Number ${seatInfo.phoneNumber}`;
 }
 
 function selectSeat(element) {
@@ -12,8 +27,9 @@ function selectSeat(element) {
         const imgID = element.classList[0].toString();
         // extracting the bus ID
         const busID = imgID.split('-')[1];
-        numberOfSelectedSeats(busID);
-    } else {
+        getSeatInfo(busID);
+    }
+    else{
         document.querySelector('.filter-green').classList.toggle('filter-green');
         element.classList.toggle('filter-green'); // why it appears green
     }
